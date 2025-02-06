2ef674cf5481272a80719b57d19e95b0bd0395f9
Jery <jery99961@gmail.com>
2025-02-06 11:23:07 +0530

    Add separate channel for sync notifications

--- app/src/main/java/eu/kanade/tachiyomi/data/sync/SyncDataJob.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/sync/SyncDataJob.kt
@@ -51,13 +51,13 @@ class SyncDataJob(private val context: Context, workerParams: WorkerParameters) 
             notifier.showSyncError(e.message)
             Result.success() // try again next time
         } finally {
-            context.cancelNotification(Notifications.ID_RESTORE_PROGRESS)
+            context.cancelNotification(Notifications.ID_SYNC_PROGRESS)
         }
     }
 
     override suspend fun getForegroundInfo(): ForegroundInfo {
         return ForegroundInfo(
-            Notifications.ID_RESTORE_PROGRESS,
+            Notifications.ID_SYNC_PROGRESS,
             notifier.showSyncProgress().build(),
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                 ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC
