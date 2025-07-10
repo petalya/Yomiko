9981dac7b63f786ca8bef938f252387f2e213585
petalya <namedaymen@gmail.com>
2025-07-10 22:05:50 +0100

    misc

--- core/common/src/main/kotlin/eu/kanade/tachiyomi/core/security/PrivacyPreferences.kt
+++ core/common/src/main/kotlin/eu/kanade/tachiyomi/core/security/PrivacyPreferences.kt
@@ -5,7 +5,7 @@ import tachiyomi.core.common.preference.PreferenceStore
 class PrivacyPreferences(
     private val preferenceStore: PreferenceStore,
 ) {
-    fun crashlytics() = preferenceStore.getBoolean("crashlytics", true)
-
-    fun analytics() = preferenceStore.getBoolean("analytics", true)
+    // REMOVE:
+    // fun crashlytics() = preferenceStore.getBoolean("crashlytics", true)
+    // fun analytics() = preferenceStore.getBoolean("analytics", true)
 }
