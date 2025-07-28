f98f4b1cb575c13348f7d55764a290b0754ebd0c
petalya <namedaymen@gmail.com>
2025-07-28 18:36:46 +0100

    fix epub4j implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -316,6 +316,7 @@ dependencies {
     implementation(sylibs.zxing.android.embedded)
 
     implementation("io.documentnode:epub4j-core:4.2.1") {
+        exclude(group = "xmlpull", module = "xmlpull")
         exclude(group = "net.sf.kxml", module = "kxml2")
     }
     implementation("com.valentinilk.shimmer:compose-shimmer:1.3.3")
